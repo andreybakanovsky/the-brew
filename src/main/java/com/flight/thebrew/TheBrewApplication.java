88b56441376d931e4b69f0765afa607bb7cc2736
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2025-02-27 18:39:42 +0300

    Init project

---
+++ src/main/java/com/flight/thebrew/TheBrewApplication.java
@@ -0,0 +1,13 @@
+package com.flight.thebrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flight/thebrew/TheBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.thebrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
