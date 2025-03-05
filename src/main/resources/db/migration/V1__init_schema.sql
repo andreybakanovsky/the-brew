037089827fae11df924c38b2c1e36089ba8188b2
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2025-03-05 12:45:03 +0300

    Add Flyway

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE coffee
+(
+    id     SERIAL PRIMARY KEY,
+    name   VARCHAR(100),
+    amount INTEGER,
+    cost   INTEGER
+);
---
+++ src/main/resources/db/migration/V2__seeds_coffee.sql
@@ -0,0 +1,5 @@
+INSERT INTO coffee (name, amount, cost)
+VALUES ('Arabica', 10, 1),
+       ('Robusta', 12, 2),
+       ('Excelsa', 14, 3),
+       ('Liberica', 17, 4)
