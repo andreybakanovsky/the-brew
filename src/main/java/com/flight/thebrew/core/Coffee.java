a655d4b18af654498b7ded2649eab4c444febf13
Andrey Bakanovsky <andrey.bakanovsky@outlook.com>
2025-03-05 09:53:04 +0300

    Add the postgres config

--- src/main/java/com/flight/thebrew/core/Coffee.java
+++ src/main/java/com/flight/thebrew/core/Coffee.java
@@ -1,16 +1,18 @@
 package com.flight.thebrew.core;
 
-import jakarta.persistence.*;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "coffee")
 public class Coffee {
     @Id
